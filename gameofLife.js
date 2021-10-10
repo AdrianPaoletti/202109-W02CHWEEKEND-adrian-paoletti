2a103009887b368099f04a05b86ce2db791d0c44
AdrianPaoletti <adriandelapao@gmail.com>
2021-10-10 16:21:15 +0200

    First alive move done

--- gameofLife.js
+++ gameofLife.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable prefer-const */
 function tableCreator(dimension) {
   const arrayCreator = [];
   for (let i = 0; i < dimension; i += 1) {
@@ -15,20 +17,27 @@ function matrixExtraction(table, vertical, horizontal) {
     arrayCreation[i] = Array(3);
     if (((vertical - 1) + i) < 0) {
       for (let z = 0; z < 3; z += 1) {
-        arrayCreation[i][z] = 0;
+        arrayCreation[i][z] = undefined;
       }
     }
     else if (((vertical - 1) + i) === table.length) {
       for (let z = 0; z < 3; z += 1) {
-        arrayCreation[i][z] = 0;
+        arrayCreation[i][z] = undefined;
       }
     }
     else {
       for (let z = 0; z < 3; z += 1) {
-        arrayCreation[i][z] = (table[(vertical - 1) + i][(horizontal - 1) + z]);
+        if (table[(vertical - 1) + i][(horizontal - 1) + z] === undefined) {
+          arrayCreation[i][z] = undefined;
+        }
+        else {
+          arrayCreation[i][z] = (table[(vertical - 1) + i][(horizontal - 1) + z]).innerHTML;
+        }
       }
     }
   }
+  /* console.log(arrayCreation);
+  console.log('---'); */
   return arrayCreation;
 }
 
@@ -49,7 +58,11 @@ function counterMatrixExtraction(matrixTable) {
   let numNeighboursRunned = 0;
   for (let i = 0; i < matrixTable.length; i += 1) {
     for (let z = 0; z < matrixTable[i].length; z += 1) {
-      if (matrixTable[i][z] === 1) {
+
+      if (matrixTable[i][z] === undefined) {
+        numNeighboursRunned += 1;
+      }
+      else if (matrixTable[i][z] === '1') {
         numOne += 1;
         numNeighboursRunned += 1;
       }
@@ -58,11 +71,11 @@ function counterMatrixExtraction(matrixTable) {
       }
       if ((numOne < 3 || numOne > 4) && numNeighboursRunned > 8) {
         // eslint-disable-next-line no-param-reassign
-        matrixTable[1][1] = 0;
+        matrixTable[1][1] = '0';
       }
-      else if (numOne === 3 && matrixTable[1][1] === 0 && numNeighboursRunned > 8) {
+      else if (numOne === 3 && matrixTable[1][1] === '0' && numNeighboursRunned > 8) {
         // eslint-disable-next-line no-param-reassign
-        matrixTable[1][1] = 1;
+        matrixTable[1][1] = '1';
       }
     }
   }
@@ -74,31 +87,77 @@ function mainTableRunner(tableOrigin) {
   for (let i = 0; i < tableOrigin.length; i += 1) {
     for (let z = 0; z < tableOrigin[i].length; z += 1) {
       const tempMatrixExtraction = matrixExtraction(tableOrigin, i, z);
+      // console.table(tempMatrixExtraction);
       const modifiedMatrix = counterMatrixExtraction(tempMatrixExtraction);
+      // console.table(modifiedMatrix);
+      // console.log('---')
       matrixInsertion(modifiedMatrix, finalTable, i, z)
     }
   }
+  // eslint-disable-next-line no-param-reassign
   return finalTable;
 }
-const tableDimension = 6;
+
+function changeTable(temporalTable, originalTable) {
+  for (let i = 0; i < originalTable.length; i += 1) {
+    for (let z = 0; z < originalTable[i].length; z += 1) {
+      originalTable[i][z].innerHTML = temporalTable[i][z];
+      if (originalTable[i][z].innerHTML === '0') {
+        originalTable[i][z].style.backgroundColor = '#7e7e7e';
+      }
+      else {
+        originalTable[i][z].style.backgroundColor = '#FFFF00';
+      }
+    }
+  }
+  return originalTable
+}
+
+// TABLE DIMENSION
+const tableDimension = 5;
 
 
-const matrixDivs = document.querySelectorAll('.matrix__square--Y > div');
-for (let i = 0; i < matrixDivs.length; i += 1) {
-  matrixDivs[i].addEventListener("click", () => {
-    matrixDivs[i].style.background = "yellow";
-  });
+// SELECTOR INTO ARRAY
+const matrixDivsX = document.querySelectorAll('.matrix__square--Y > div');
+let matrixSelector = tableCreator(tableDimension);
+let contador = 0;
+for (let i = 0; i < matrixSelector.length; i += 1) {
+  for (let z = 0; z < matrixSelector[i].length; z += 1) {
+    matrixSelector[i][z] = matrixDivsX[contador];
+    matrixSelector[i][z].innerHTML = '0';
+    contador += 1;
+  }
+}
+
+// COLOR YELLOW
+for (let i = 0; i < matrixSelector.length; i += 1) {
+  for (let z = 0; z < matrixSelector[i].length; z += 1) {
+    // eslint-disable-next-line no-loop-func
+    matrixSelector[i][z].addEventListener("click", () => {
+      matrixSelector[i][z].style.background = "#FFFF00";
+      matrixSelector[i][z].innerHTML = '1';
+    });
+  };
 };
 
+// BUTTON CLEAR
 const clearMatrix = document.querySelector('.footer__button--clear');
 clearMatrix.addEventListener('click', () => {
-  for (let i = 0; i < matrixDivs.length; i += 1) {
-    matrixDivs[i].style.background = '#7e7e7e'
+  for (let i = 0; i < matrixSelector.length; i += 1) {
+    for (let z = 0; z < matrixSelector[i].length; z += 1) {
+      matrixSelector[i][z].style.backgroundColor = '#7e7e7e';
+      matrixSelector[i][z].innerHTML = '0';
+    }
   }
-
 })
 
 
+// BUTTON PLAY
+let playMatrix = document.querySelector('.footer__button--play');
+playMatrix.addEventListener('click', () => {
+  let changedTable = mainTableRunner(matrixSelector)
+  changeTable(changedTable, matrixSelector);
+})
 
 
 
